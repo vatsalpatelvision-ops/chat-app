b27852199c60b491ac438ec8dcde250fca398e4d
vatsalpatelvision-ops <vatsalpatel.vision@gmail.com>
2026-05-01 15:20:07 +0530

    web soket group chat

---
+++ chatapplication/chatapplication/asgi.py
@@ -0,0 +1,21 @@
+"""
+ASGI config for chatapplication project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter,URLRouter
+from chat import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
+
+application = ProtocolTypeRouter({
+    "http":get_asgi_application(),
+    "websocket":URLRouter(routing.websocket_urlpatterns)
+})
